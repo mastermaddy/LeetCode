e05620aaa080c6f992d9374878c3d5cfb51be387
Vrushabh Madavi <vrushabhmadavi@gmail.com>
2022-04-09 06:48:44 +0530

    Create implement-stack-using-queues.cpp

---
+++ implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+class MyStack {
+private:
+	queue<int> que;
+public:
+	MyStack() {}
+
+	void push(int x) {
+		que.push(x);
+		for(int i=0;i<que.size()-1;++i){
+			que.push(que.front());
+			que.pop();
+		}
+	}
+
+	// Removes the element on top of the stack.
+	int pop() {
+		int tp = top();
+		que.pop();
+		return tp;
+	}
+
+	// Get the top element.
+	int top() {
+		return que.front();
+	}
+
+	// Return whether the stack is empty.
+	bool empty() {
+		return que.empty();
+	}
+};
+
