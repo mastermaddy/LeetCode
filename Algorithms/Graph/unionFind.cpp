4854ec0c777ffb1d5ada12d902002c042adf5044
Vrushabh Madavi <vrushabhmadavi@gmail.com>
2022-06-11 03:19:06 +0530

    Create unionFind.cpp

---
+++ Algorithms/Graph/unionFind.cpp
@@ -0,0 +1,36 @@
+class UnionFind{
+private:
+    int numOfNodes;
+    vector<int> sizeOfCluster;
+    vector<int> parentOfCluster;
+public:
+    UnionFind(int n){
+        numOfNodes = n;
+        sizeOfCluster.resize(numOfNodes,1);
+        parentOfCluster.resize(numOfNodes);
+        for(int i=0;i<numOfNodes;++i){
+            parentOfCluster[i] = i;
+        }
+    }
+    
+    int Find(int node){
+        if(parentOfCluster[node]!=node){
+            parentOfCluster[node] = Find(parentOfCluster[node]);
+        }
+        return parentOfCluster[node];
+    }
+    
+    bool Union(int node1, int node2){
+        int parent1 = Find(node1);
+        int parent2 = Find(node2);
+        if(sizeOfCluster[parent1]>sizeOfCluster[parent2]){
+            parentOfCluster[parent2] = parent1;
+            sizeOfCluster[parent1] += sizeOfCluster[parent1];
+        }
+        else if(sizeOfCluster[parent1]<=sizeOfCluster[parent2]){
+            parentOfCluster[parent1] = parent2;
+            sizeOfCluster[parent2] += sizeOfCluster[parent1];
+        }
+        return parent1 == parent2;
+    }
+};
