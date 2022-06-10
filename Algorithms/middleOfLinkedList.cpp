92d417a5f22141e5fbbb2ed6c0e80ed850c47f53
Vrushabh Madavi <vrushabhmadavi@gmail.com>
2022-06-11 03:19:26 +0530

    Delete middleOfLinkedList.cpp

--- Algorithms/middleOfLinkedList.cpp
+++
@@ -1,8 +0,0 @@
-ListNode* getMiddleOfList(ListNode* head, ListNode* tail = nullptr){
-    ListNode* fast = head, *slow = head;
-    while(fast!=tail && fast->next!=tail){
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    return slow;
-}
