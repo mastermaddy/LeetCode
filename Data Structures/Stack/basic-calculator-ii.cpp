d819b2c54b50bf0f675934248414f0133a284840
Vrushabh Madavi <vrushabhmadavi@gmail.com>
2022-06-18 02:06:48 +0530

    Update basic-calculator-ii.cpp

--- Data Structures/Stack/basic-calculator-ii.cpp
+++ Data Structures/Stack/basic-calculator-ii.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> evaluation;
-        int num = 0, total = 0, sz = s.size();
+        int total = 0, num = 0, sz = s.size();
         char sign = '+';
+        stack<int> eval;
         for(int i=0;i<sz;++i){
             if(isdigit(s[i])){
                 num = num*10 + (s[i] - '0');
             }
-            if(!isdigit(s[i]) && s[i]!=' ' || i == sz-1){
+            if(!isdigit(s[i]) && s[i]!=' ' || i==sz-1){
                 if(sign=='+'){
-                    evaluation.push(num);
+                    eval.push(num);
                 }
                 else if(sign=='-'){
-                    evaluation.push(-num);
+                    eval.push(-num);
                 }
                 else{
-                    if(sign=='*') num = evaluation.top()*num;
-                    else if(num!=0) num = evaluation.top()/num;
-                    evaluation.pop();
-                    evaluation.push(num);
+                    if(sign=='*'){
+                        num = eval.top()*num;
+                    }
+                    else{
+                        num = eval.top()/num;
+                    }
+                    eval.pop();
+                    eval.push(num);
                 }
                 num = 0;
                 sign = s[i];
             }
         }
-        while(!evaluation.empty()){
-            total += evaluation.top();
-            evaluation.pop();
+        while(!eval.empty()){
+            total += eval.top();
+            eval.pop();
         }
         return total;
     }
