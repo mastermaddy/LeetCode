9e0c408b333d13fbe8c9d4fb6301293fc4549280
Vrushabh Madavi <vrushabhmadavi@gmail.com>
2022-06-17 12:31:03 +0530

    Update find-peak-element.cpp

--- Algorithms/Binary Search/find-peak-element.cpp
+++ Algorithms/Binary Search/find-peak-element.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int numSize = nums.size();
-        if(numSize>1){
-            if(nums[0]>nums[1]){
-                return 0;
+        int left = 0, right = numSize-1;
+        while(left<right){
+            int mid = (left + right)/2;
+            if(nums[mid]>nums[mid+1]){
+                right = mid;
             }
-            if(nums[numSize-1]>nums[numSize-2]){
-                return numSize-1;
-            }
-            int left = 1, right = numSize-2;
-            while(left<=right){
-                int mid = (left + right)/2;
-                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                    return mid;
-                }
-                if(nums[mid]<nums[mid-1]){
-                    right = mid - 1;
-                }
-                else if(nums[mid]<nums[mid+1]){
-                    left = mid + 1;
-                }
+            else{
+                left = mid+1;
             }
         }
-        return numSize==1?0:-1;
+        return left;
     }
 };
